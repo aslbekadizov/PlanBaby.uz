45a4446d12a9eb2b98bc9fd5fbd8aec8d7b2e3dc
aslbekadizov2001 <aslbekadizov2001@gmail>
2022-10-20 19:48:49 +0500

    first

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 window.addEventListener('load', function(e){
-    var header_title_right = document.querySelector(".header_title_right img") 
-    var frame = document.querySelector(".frame")
 
-    header_title_right.addEventListener('click', function(){
-        frame.style.display = 'inline-flex'
+//so'zni chiqishi
+
+    let infoElements_p = document.querySelector(".info_elements p");
+    let info_elements = document.querySelector(".info_elements")
+    let word_p = infoElements_p.innerHTML;
+    let i = 0;
+
+    infoElements_p.innerHTML = '';
+    
+    info_elements.addEventListener('mouseover', function(){
+        function print(){
+            infoElements_p.innerHTML += word_p.charAt(i)
+            i++
+        }
+        setInterval(print, 20)
     })
+//so'z chiqishi tugadi
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // var header_title_right = document.querySelector(".header_title_right img") 
+    // var frame = document.querySelector(".frame")
+
+    // header_title_right.addEventListener('click', function(){
+    //     frame.style.display = 'inline-flex'
+    // })
 })
